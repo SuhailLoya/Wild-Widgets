cd05cce2f9f59fd3eba411c2166150df0baf6729
Kumar <113902321+kumar2215@users.noreply.github.com>
2024-01-21 00:58:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
 import tkinter as tk
-import pyautogui as pg
-from PIL import ImageTk, Image
 import time
 
 root = tk.Tk()
 root.configure(bg='#ffa500')
-root.geometry('1600x1200')
-root.title('Annoying app') # placeholder
+WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry(f'{WIDTH}x{HEIGHT}')
+root.title('Annoying app')
 
-# title = tk.Label(root, text='Welcome to Chem Lab! \n\nSelect what you want to do: ', font=('Arial', 25),
-#                  bg='#ffa500')
-# title.pack(padx=50, pady=60)
+def nothing(): return
+root.protocol('WM_DELETE_WINDOW', nothing)
 
-def button1redirect():
-    pass
+def top_left_redirect():
+    root.destroy()
+    import trial
 
-def button2redirect():
-    pass
+def top_right_redirect():
+    root.destroy()
+    import mouse_chase
 
-Top_Left = tk.Canvas(root, bg='#ff0000')
-Top_Left.place(x=0, y=0, width=800, height=600)
+def bottom_left_redirect():
+    root.destroy()
+    # import trial
 
-Top_Right = tk.Canvas(root, bg='#3cb371')
-Top_Right.place(x=800, y=0, width=800, height=600)
+def bottom_right_redirect():
+    root.destroy()
+    # import game2
 
-Bottom_Left = tk.Canvas(root, bg='#eee7da')
-Bottom_Left.place(x=0, y=600, width=800, height=600)
+Top_Left = tk.Button(root, bg='#ff0000', command=top_left_redirect)
+Top_Left.place(x=0, y=0, width=WIDTH/2, height=HEIGHT/2)
 
-Bottom_Right = tk.Canvas(root, bg='#fad7a0')
-Bottom_Right.place(x=800, y=600, width=800, height=600)
+Top_Right = tk.Button(root, bg='#3cb371', command=top_right_redirect)
+Top_Right.place(x=WIDTH/2, y=0, width=WIDTH/2, height=HEIGHT/2)
 
-# click_btn1 = ImageTk.PhotoImage(Image.open("Chemistry.jpg"))
-# button1 = tk.Button(root, bg='#ff0000', command=button1redirect,
-#                  text='\n\n\n\n\n\n\n  Get info on   \n     a molecule     ', font=('Arial', 23))
-# button1.place(x=0, y=300, height=300, width=300)
+Bottom_Left = tk.Button(root, bg='#eee7da', command=bottom_left_redirect)
+Bottom_Left.place(x=0, y=HEIGHT/2, width=WIDTH/2, height=HEIGHT/2)
 
-# click_btn2 = ImageTk.PhotoImage(Image.open("structural_elucidation.jpg"))
-# button2 = tk.Button(root, image=click_btn2, bg='#3cb371', command=button2redirect,
-#                  text='\n\n\n\n\n\n\n  Solve a structural \nelucidation problem', font=('Arial', 23))
-# button2.place(x=300, y=300, height=300, width=300)
+Bottom_Right = tk.Button(root, bg='#fad7a0', command=bottom_right_redirect)
+Bottom_Right.place(x=WIDTH/2, y=HEIGHT/2, width=WIDTH/2, height=HEIGHT/2)
 
 root.mainloop()
-
-
